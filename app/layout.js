36474edc53b2760c9f4cef69ea555ed752764334
sebastian <ssp013sanchez@gmail.com>
2023-08-15 16:57:24 -0400

    fixed

--- app/layout.js
+++ app/layout.js
@@ -11,7 +11,7 @@ export const metadata = {
 
 export default function RootLayout ({ children }) {
   return (
-    <html lang='en' className='dark'>
+    <html lang='en'>
       <head>
         <title>Marina Market App</title>
       </head>
