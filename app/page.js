7daed5182c82851707ecea8cdbf9e9b0d9c0a582
sebastian <ssp013sanchez@gmail.com>
2023-08-14 20:02:19 -0400

    home page

--- app/page.js
+++ app/page.js
@@ -1,6 +1,6 @@
 /* Aqui debemos redirigir entre el home o el login */
 export default function Home () {
   return (
-    <h1>Esta es nuestra app por ahora</h1>
+    <h1>This is my Home Page</h1>
   );
 }
