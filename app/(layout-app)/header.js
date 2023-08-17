50afdf491ac946662c1bcf3b0175cf2208e49d09
sebastian <ssp013sanchez@gmail.com>
2023-08-16 23:41:44 -0400

    Added new layout-app

--- app/(layout-app)/header.js
+++ app/(layout-app)/header.js
@@ -3,9 +3,9 @@ import ThemeButton from '@/components/ui/ThemeButton'
 import { HomeButton } from '@/components/ui/HomeButton';
 export function Header () {
   return (
-    <header className={'sticky z-20 bg-primary-200 dark:bg-secondary-500 top-0 flex  flex-row-reverse mt-[1rem] mx-[9.5rem] mb-[1rem] space-x-3'}>
+    <section className={'flex flex-row-reverse mt-[1rem] mx-[13rem] space-x-3'}>
       <HomeButton/>
       <ThemeButton/>   
-    </header>
+    </section>
   );
 }
--- app/(layout-app)/layout.js
+++ app/(layout-app)/layout.js
@@ -4,17 +4,15 @@ import  { Header }  from "./header";
 export default function LayoutApp ({ children }) {
   return (
     <section className="min-h-screen w-full h-full flex flex-col bg-primary-200 dark:bg-secondary-500">
-    <Header/>
-    <div className="flex-1 flex flex-col sm:flex-row  overflow-y-scroll">
-      <main className="flex-1">
+    <header className="sticky z-20 bg-primary-200 dark:bg-secondary-500 top-0 h-[6rem]">
+      <Header/>
+    </header>
+    <div className="flex-1 flex flex-col sm:flex-row  pt-[1rem]">
+      <main className="flex-1 mx-[1rem] xl:mx-[14rem]">
         {children}
       </main>
-      {/* Sidebar */}
-      <nav className="order-first sm:w-[10rem]"></nav>
-      {/* Right Sidebar */}
-      <aside className="sm:w-[10rem]"></aside>
     </div>
-    <footer className="sticky z-10 bg-primary-200 dark:bg-secondary-500 bottom-0 h-[4rem] sm:h-[8rem]">
+    <footer className="sticky z-10 bg-primary-200 dark:bg-secondary-500 bottom-0 h-[6rem] pt-[1rem]">
       <Navigation/>
     </footer>
     </section>
