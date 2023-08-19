55d2a60769ccc321f33b6db67d68d18b56157e82
sebastian <ssp013sanchez@gmail.com>
2023-08-18 23:15:36 -0400

    Fixed store state user

--- components/ui/UserAvatar.js
+++ components/ui/UserAvatar.js
@@ -1,15 +1,22 @@
 'use client'
-import React from "react";
+import React,{ useState,useEffect } from "react";
 import { Card, CardHeader, Avatar, Button } from "@nextui-org/react";
 import useAuthStore from "@/stores/user";
 import ThemeButton from "./ThemeButton";
 import ShortcutButton from "./ShortcutButton";
 
 export default function UserAvatar() {
-  const { fullName: userName, isAdmin } = useAuthStore(({ fullName, isAdmin }) => ({ fullName, isAdmin }))
+  const [userName,setUserName ] = useState(null)
+  const [admin,setAdmin ] = useState(false)
+  const { fullName, isAdmin } = useAuthStore(({ fullName, isAdmin }) => ({ fullName, isAdmin }))
 
   const { signOut } = useAuthStore(({ signOut }) => ({ signOut }))
-
+useEffect(()=>{
+  if(fullName){
+    setUserName(fullName)
+    setAdmin(isAdmin)
+  }
+},[fullName])
   return (
     <div className="flex w-full">
       <Card className="flex gap-1">
@@ -17,8 +24,8 @@ export default function UserAvatar() {
         <div className="flex gap-5">
           <Avatar isBordered color="warning" radius="full" size="md" src="/avatars/avatar-1.png" />
           <div className="flex flex-col gap-1 items-start justify-center">
-            <h4 className="text-small font-semibold leading-none text-default-600">{userName?.toUpperCase()}</h4>
-            <h5 className="text-small tracking-tight text-default-400">{isAdmin ? 'Administrador' : 'Trabajador'}</h5>
+            <h4 className="text-small font-semibold leading-none text-default-600">{userName?.toUpperCase() }</h4>
+            <h5 className="text-small tracking-tight text-default-400">{admin ? 'Administrador' : 'Trabajador'}</h5>
           </div>
         </div>
         <div className="col-start-1 col-end-1">
