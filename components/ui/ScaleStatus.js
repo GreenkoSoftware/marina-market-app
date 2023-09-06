11e94d3f7a3eaa9477b008874bd3f65479eabcda
Dionisio Olivares <dionisio.olivares23@gmail.com>
2023-09-06 12:31:38 -0300

    fix popover

--- components/ui/ScaleStatus.js
+++ components/ui/ScaleStatus.js
@@ -5,52 +5,53 @@ import { FaWeight } from 'react-icons/fa'
 
 export default function ScaleStatus ({ scaleStatus }) {
     const [isOpen, setIsOpen] = useState(null)
+    const [color, setColor] = useState('danger')
+
     useEffect(() => {
-        if (isOpen) {
+        if (isOpen && scaleStatus) {
             setTimeout(
                 () => setIsOpen(false), 5000
             )
         }
-    }, [isOpen])
+    }, [isOpen, scaleStatus])
 
     useEffect(() => {
         setTimeout(
             () => setIsOpen(true), 1000
         )
+
+        if (scaleStatus) {
+            setColor('success')
+        } else {
+            setColor('danger')
+        }
     }, [scaleStatus])
 
+    const Message = ({ enabled }) => {
+        return enabled
+            ? <div className="">
+                <div className="text-small text-white font-bold">Pesa conectada</div>
+            </div>
+            : <div className="">
+                <div className="text-small font-bold">Pesa desconectada</div>
+                <div className="text-tiny">Por favor abrir el archivo scaleConnector en escritorio.</div>
+            </div>
+    }
+
     return (
-        <div className="flex items-center gap-4">
+        <div className="flex items-center gap-4" onClick={() => setIsOpen(!isOpen)}>
             <div className="flex items-center gap-3">
-                {scaleStatus
-                    ? <Popover placement="top-end" offset={25} color="success" showArrow={true} isOpen={isOpen}>
-                        <PopoverTrigger>
-                            <Badge color="success" content={''} size = "lg" shape="circle">
-                                <FaWeight size={35} />
-                            </Badge>
-                        </PopoverTrigger>
-                        <PopoverContent color="danger">
-                            <div className="">
-                                <div className="text-small text-white font-bold">Pesa conectada</div>
-                            </div>
-                        </PopoverContent>
+                <Popover placement="top-end" offset={25} color={color} showArrow={true} isOpen={isOpen}>
+                    <PopoverTrigger>
+                        <Badge color={color} content={''} size = "lg" shape="circle">
+                            <FaWeight size={35} />
+                        </Badge>
+                    </PopoverTrigger>
+                    <PopoverContent className='mt-1' color={color}>
+                        <Message enabled={scaleStatus}/>
+                    </PopoverContent>
 
-                    </Popover>
-                    : <Popover placement="top-end" offset={25} color="danger" showArrow={true} defaultOpen={true}>
-                        <PopoverTrigger>
-                            <Badge color="danger" content={''} size = "lg" shape="circle">
-                                <FaWeight size={30} />
-                            </Badge>
-                        </PopoverTrigger>
-                        <PopoverContent color="danger">
-                            <div className="">
-                                <div className="text-small font-bold">Pesa desconectada</div>
-                                <div className="text-tiny">por favor abrir el archivo, scaleConnector en escritorio.</div>
-                            </div>
-                        </PopoverContent>
-
-                    </Popover>
-                }
+                </Popover>
             </div>
         </div>
     )
