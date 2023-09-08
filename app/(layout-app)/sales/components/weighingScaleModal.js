61ca078f761319a88d0e1c217045cb27e44ed882
Nicolas Garcia Santelices <ngarcia1621@gmail.com>
2023-09-08 20:26:19 -0300

    Added manual mode in scale modal

--- app/(layout-app)/sales/components/weighingScaleModal.js
+++ app/(layout-app)/sales/components/weighingScaleModal.js
@@ -1,12 +1,14 @@
 'use client'
 /* eslint-disable */
 import React,{useState,useEffect} from 'react'
-import { Modal, ModalContent, ModalHeader, ModalBody, ModalFooter, Button, Card, CardBody, Image } from '@nextui-org/react'
+import { Modal, ModalContent, ModalHeader, ModalBody, ModalFooter, Button, Card, CardBody, Image,Input } from '@nextui-org/react'
 import { ConvertBytesToImage, DefaultImageMarinaMarket } from '@/utils/image'
 import useWebSocket, { ReadyState } from 'react-use-websocket'
 import useAuthStore from '@/stores/user'
 import { getIdUser } from '@/services/user'
 import hubScale from './store/connectionScale'
+import { LuTextCursorInput } from 'react-icons/lu'
+import { FaWeight } from 'react-icons/fa'
 
 const onSubmitHandler = (product, value, setIsAcepted, setUnits,setTargetProduct,setSelectedKL) => {
     product.price = Math.round(product?.price * value)
@@ -18,6 +20,7 @@ const onSubmitHandler = (product, value, setIsAcepted, setUnits,setTargetProduct
 export default function WeighingScaleModal ({ isOpen, onClose, product, setIsAcepted, setUnits, setTargetProduct,setSelectedKL }) {
     const [handShake,setHandShake] = useState(false)
     const [ url, setUrl ] = useState(null)
+    const [ manualMode, setManualMode ] = useState(false)
     const {setIsConnected} = hubScale()
     const[value,setValue] = useState(1)
     const {
@@ -73,15 +76,24 @@ export default function WeighingScaleModal ({ isOpen, onClose, product, setIsAce
                 <ModalContent className=''>
                     {(onClose) => (
                         <>
-                            <ModalHeader className="flex flex-col gap-1  text-primary-500 dark:text-primary-200 ">Pesa de productos</ModalHeader>
-                            <ModalBody>
+                            <ModalHeader className="flex flex-col gap-1 text-primary-500 dark:text-primary-200 ">Pesa de productos
+                            <Button color="success" variant="ghost" className='w-full'
+                                        onClick={() =>  {manualMode?setManualMode(false):setManualMode(true)}}
+                            >
+                                        {manualMode?
+                                        "MODO MANUAL"
+                                        :
+                                        "MODO AUTOMATICO"}
+                            </Button>
+                            </ModalHeader>
+                            <ModalBody className='flex flex-col '>
                                 <Card
                                     isBlurred
                                     className="border-none w-full"
                                     shadow="sm"
                                 >
                                     <CardBody>
-                                        <div className="flex flex-row gap-10 items-center justify-center content-between">
+                                        <div className="flex flex-row gap-10   content-between">
                                             <div className="col-span-6 md:col-span-4 dark:bg-default-100/50 bg-default-100/70 rounded-xl">
                                                 <Image
                                                     shadow="none"
@@ -101,26 +113,44 @@ export default function WeighingScaleModal ({ isOpen, onClose, product, setIsAce
                                                         </div>
                                                 </div>
                                             </div>
-
-                                            <div className="flex flex-col col-span-6 md:col-span-8 h-full">
-                                                <div className="flex  items-start h-full">
+                                            {manualMode?
+                                            <div className="flex flex-col items-center justify-center content-between gap-2 h-full">
+                                                <div className="flex  items-start h-full w-full">
                                                     <div className="flex flex-col gap-5 h-full">
-                                                        <div className='flex flex-col  gap-2 h-full'>
-                                                            <h1 className="text-large font-medium mt-2">PESO</h1>
+                                                        <div className='flex flex-col items-center gap-2 h-full'>
+                                                            <h1 className="text-large font-medium mt-2">PESO REGISTRADO</h1>
                                                             <div className='flex flex-row gap-1 justify-center items-end'>
                                                                 <h1 className="text-8xl text-teal-500 font-medium ">{value}</h1>
                                                                 <h1 className="text-lg text-zinc-400 font-medium ">KG</h1>
                                                             </div>
                                                         </div>
-                                                        
-                                                     
-                                                            <div className='flex flex-row w-full gap-4 items-center px-5 py-3 border-3 mt-6 rounded-xl border-green-500'>
-                                                                <h1 className="text-4xl  text-green-500 font-bold">TOTAL</h1>
-                                                                <h1 className="text-4xl font-xl font-bold text-green-500">$ {Math.floor((product?.netPrice * value) / 10) *10}</h1>
-                                                            </div>
+                                                        <div className='flex flex-row w-[20rem] gap-4 items-center px-5 py-3 border-3 mt-6 rounded-xl border-green-500'>
+                                                            <h1 className="text-4xl  text-green-500 font-bold">TOTAL</h1>
+                                                            <h1 className="text-4xl font-xl font-bold text-green-500">$ {Math.floor((product?.netPrice * value) / 10) *10}</h1>
+                                                        </div>
                                                     </div>
                                                 </div>
-                                            </div>
+                                            </div>:
+                                            <section className="flex flex-col items-center justify-center content-between gap-2 h-full">
+                                                <div key="flat" className="flex w-[20rem] h-[4rem] flex-wrap md:flex-nowrap mb-6 md:mb-0 gap-4">
+                                                    <Input type="email" variant={"flat"} label="kg" placeholder="Ingresa manualmente los kg" className='text-sm'
+                                                        onValueChange={(value)=>setValue(value)}
+                                                    />
+                                                </div>
+                                                <Button className=' w-[20rem] h-[3rem] bg-green-600 text-white font-bold text-lg'
+                                                    onClick={() => setValue(1) }>
+                                                    1KG
+                                                </Button>
+                                                <Button className=' w-[20rem] h-[3rem]  bg-green-600 text-white font-bold text-lg'
+                                                    onClick={() => setValue(2) }>
+                                                    2KG
+                                                </Button>
+                                                <div className='flex flex-row w-[20rem] gap-4 items-center px-5 py-3 border-3 rounded-xl border-green-500'>
+                                                    <h1 className="text-4xl  text-green-500 font-bold">TOTAL</h1>
+                                                    <h1 className="text-4xl font-xl font-bold text-green-500">$ {Math.floor((product?.netPrice * value) / 10) *10}</h1>
+                                                </div>
+                                            </section>
+                                            }
                                         </div>
                                     </CardBody>
                                 </Card>
